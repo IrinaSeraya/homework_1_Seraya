b59036585535a1018417c07007430859e84ab4ba
Seraya <irina.seraya99@mail.ru>
2025-04-01 00:10:08 +0300

    Changes

--- task_1.py
+++
@@ -1,6 +0,0 @@
-# Задание 1
-user_last_name = input('Введите фамилию ')
-user_first_name = input('Введите имя ')
-user_middle_name = input('Введите отчество ')
-print(user_last_name, user_first_name[0] + '.' + user_middle_name[0] + '.')
-
