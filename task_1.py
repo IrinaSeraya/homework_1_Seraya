1d97e70e00b7c5f3467ee0a992ef11e59996e2da
Seraya <irina.seraya99@mail.ru>
2025-04-01 00:22:32 +0300

    deleting lines

--- task_1.py
+++ task_1.py
@@ -1,6 +1,5 @@
 # Задание 1
-user_last_name = input('Введите фамилию ')
-user_first_name = input('Введите имя ')
-user_middle_name = input('Введите отчество ')
-print(user_last_name, user_first_name[0] + '.' + user_middle_name[0] + '.')
-
+user_last_name = input('Введите фамилию ') #Ввод фамилии
+user_first_name = input('Введите имя ')#Ввод имени
+user_middle_name = input('Введите отчество ') #Ввод отчества
+print(user_last_name, user_first_name[0] + '.' + user_middle_name[0] + '.') #Вывод в формате Фамилия И.О.
--- task_7.py
+++ task_7.py
@@ -1,5 +1,4 @@
 user_input = int(input('Введите количество секунд '))
-minutes = user_input // 60
-seconds = user_input % 60
+minutes = user_input // 60 #Кол-во минут
+seconds = user_input % 60 #Кол-во секунд
 print(str(minutes) + ' минут(-а) и ' + str(seconds) + ' секунд(-а)') 
-#Не уверена, надо ли было выводить количество секунд, если кол-во градусов не кратно 6
