ab9a9d77a1916ebc43ba6e1971c2de54f8dac38d
Seraya <irina.seraya99@mail.ru>
2025-04-09 00:43:33 +0300

    Add task 5

---
+++ homework_2/task_5.py
@@ -0,0 +1,10 @@
+#Пользователь вводит 2 слова. Напишите программу, которая проверяет, являются ли они анаграммами 
+#(первое слово может быть сформировано путем перестановки букв во втором слове)
+user_input_1 = (input('Введите первое слово '))
+user_input_2 = (input('Введите второе слово '))
+set_of_symbols_1 = set(user_input_1)
+set_of_symbols_2 = set(user_input_2)
+if set_of_symbols_1 == set_of_symbols_2:
+    print('Слова ', user_input_1, ' и ', user_input_2, ' являются анаграммами')
+else:
+    print('Слова ', user_input_1, ' и ', user_input_2, ' не являются анаграммами')
