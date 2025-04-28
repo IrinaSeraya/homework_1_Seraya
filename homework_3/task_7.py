ee96ac2fcfca243b3268de3cdfa3014ea50b2030
Seraya <irina.seraya99@mail.ru>
2025-04-29 01:44:47 +0300

    Task 7 without sorted

--- homework_3/task_7.py
+++ homework_3/task_7.py
@@ -1,18 +1,16 @@
-# Реализуйте функцию merge_sorted_list, которая принимает два отсортированных списка, и возвращает новый отсортированный список, содержащий элементы из обоих списков.
+# Реализуйте функцию merge_sorted_list, которая принимает два отсортированных списка, 
+# и возвращает новый отсортированный список, содержащий элементы из обоих списков.
 from heapq import merge
 def merge_sorted_list (list_1, list_2):
-    return sorted(list_1 + list_2)
+    merge_sorted_list = []
+    for i in list_1:
+        for j in list_2:
+            if i == j:
+                merge_sorted_list.append(i)
+    return merge_sorted_list
     #merge_sorted_list = merge (list_1.sort(), list_2.sort())
 
-spis_1 = list(input().split())
-spis_2 = list(input().split())
+spis_1 = list(input('Введите первый список').split())
+spis_2 = list(input('Введите второй список').split())
 new_merge_sorted_list = merge_sorted_list(spis_1, spis_2)
 print(new_merge_sorted_list)
-
-def unique_elements(lst):
-    unique_list = []
-    for i in lst:
-        if i not in unique_list:
-            unique_list.append(i)
-    return unique_list
-print(unique_elements(new_merge_sorted_list))
