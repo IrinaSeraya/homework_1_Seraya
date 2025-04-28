3b450b1d8260ebd824b3be20c1671ba89c333f70
Seraya <irina.seraya99@mail.ru>
2025-04-29 00:43:27 +0300

    Add text in file

--- homework_3/task_3.py
+++ homework_3/task_3.py
@@ -1,12 +1,18 @@
 #Напишите декоратор log_calls, который записывает в файл время вызова, имя и аргументы вызванной функции. 
-# Один вызов функции - одна строка в файле. Декоратор должен принимать имя файла для записи в качестве параметра.
+# Один вызов функции - одна строка в файле. 
+# Декоратор должен принимать имя файла для записи в качестве параметра. - С этим не очень понятно...
 from functools import  wraps
 import inspect
-import time
+import datetime
 def log_calls (func):
+    @wraps(func)
     def wrapper (*args, **kwargs):
         result = func(*args, **kwargs)
-        print(f"Время вызова: {time.gmtime(time.time())}")
+        fh = open('log_calls.txt', 'w')
+        fh.write(f"Время вызова: {datetime.datetime.now()}" + f" Вызвана функция: {func.__name__}"
+                 + f" Получены аргументы: {inspect.signature(func)}" + "\n")
+        fh.close()
+        print(f"Время вызова: {datetime.datetime.now()}")
         print(f"Вызвана функция: {func.__name__}")
         print(f"Получены аргументы: {inspect.signature(func)}")
         
@@ -17,3 +23,5 @@ def add(a, b):
     print(a+b)
 
 add(5, 10)
+with open('log_calls.txt', 'r') as fh:
+  print(fh.readlines())
