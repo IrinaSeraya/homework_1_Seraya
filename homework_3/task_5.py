764a13fa7e102bbe3695caeb0ea245c831e340e5
Seraya <irina.seraya99@mail.ru>
2025-04-29 01:30:20 +0300

    Task 5 + kwargs

--- homework_3/task_5.py
+++ homework_3/task_5.py
@@ -1,13 +1,17 @@
 # Напишите декоратор cache, который кэширует результат вызова функции (сохраняет в словаре). 
 # Когда функция вызывается снова с теми же аргументами, декоратор должен возвращать результат из кэша, 
 # вместо того, чтобы вызывать декорированную функцию.
+from functools import wraps
 def cache_decorator(func):
     cache = {}
-    def wrapper(*args):
-        if args in cache:
-            return cache[args]
-        result = func(*args)
-        cache[args] = result
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        cache_key = args + tuple(kwargs.items())
+        if cache_key in cache:
+            result = cache[cache_key]
+        else:
+            result = func(*args)
+            cache[cache_key] = result
         return result
     return wrapper
 
