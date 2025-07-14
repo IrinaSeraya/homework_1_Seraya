f6ef03759fe14b35a0a966939b33dca110e788d0
Seraya <irina.seraya99@mail.ru>
2025-07-14 21:11:52 +0300

    Task_3

---
+++ task_3.py
@@ -0,0 +1,7 @@
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+letter = input('Введите букву ')
+shift = int(input('Сдвиг на '))
+result = ''
+for c in letter:
+    result += alphabet[(alphabet.index(c) + shift) % len (alphabet)]
+print ('Итог: '+ result)
